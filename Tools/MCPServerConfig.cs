127705008cf21bacd2ed7182b76a6a01b57b6933
Corey Skiffington <fwcorey@gmail.com>
2025-12-30 10:39:13 +1100

    Added DescriptionPath config field
    Also added intellisense to all fields

--- Tools/MCPServerConfig.cs
+++ Tools/MCPServerConfig.cs
@@ -1,5 +1,17 @@
 public static class MCPServerConfig
 {
+    /// <summary>
+    /// Contains the relative path to the volume description
+    /// </summary>
+    public static string DescriptionPath { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Root path of the volume
+    /// </summary>
     public static string RootPath { get; set; } = Environment.CurrentDirectory;
+
+    /// <summary>
+    /// TCP Port to use when server transport is http
+    /// </summary>
     public static ushort HttpPort { get; set; } = 5000;
 }
